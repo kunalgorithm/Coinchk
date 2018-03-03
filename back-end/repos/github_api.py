dff9185ec83fdb54c10a147c90802c059c3e6ffe
riceluxs1t <riceluxs1t@gmail.com>
2018-03-03 14:14:23 -0600

    top 10 data + github api finalize.d

--- back-end/repos/github_api.py
+++ back-end/repos/github_api.py
@@ -1,7 +1,9 @@
 from apiserver.settings import github_object
 
-import datetime
+from datetime import datetime
 import requests
+
+LIMIT = 5
 
 
 def get_repo(coin_name):
@@ -25,20 +27,24 @@ def get_num_watchers(repo):
 
 
 def get_readme_content_and_lineno(repo):
-    readme = repo.get_readme()
-    download_url = readme.raw_data['download_url']
-    readme_raw_text = requests.get(download_url).text.split("\n'")
+    try:
+        readme = repo.get_readme()
+        download_url = readme.raw_data['download_url']
+        readme_raw_text = requests.get(download_url).text.split("\n")
+    except:
+        return None, 0
     return readme_raw_text, len(readme_raw_text)
 
 
 def get_commits_and_contributors(repo):
     commits = repo.get_commits()
-
     contributors_data = {}
-    commits_data = {}
+    commits_data = []
     commit_id = 0
-    for commit in commits[::-1]:
+    for commit in commits:
         c_stats = commit.stats
+        if commit.author is None:
+            continue
         author_name = commit.author.login
         num_lines_added = c_stats.additions
         num_lines_deleted = c_stats.deletions
@@ -52,16 +58,165 @@ def get_commits_and_contributors(repo):
                 'num_lines_edited': 0
             }
 
-        commits_data[commit_id] = {
-            'date': commit_date,
-            'num_lines_added': num_lines_added,
-            'num_lines_deleted': num_lines_deleted,
-            'num_lines_edited': num_lines_added + num_lines_deleted
-        }
+        commits_data.append(
+            {
+                'id': commit_id,
+                'date': commit_date,
+                'num_lines_added': num_lines_added,
+                'num_lines_deleted': num_lines_deleted,
+                'num_lines_edited': num_lines_added + num_lines_deleted
+            }
+        )
 
         contributors_data[author_name]['num_lines_added'] += num_lines_added
         contributors_data[author_name]['num_commits'] += 1
         contributors_data[author_name]['num_lines_deleted'] += num_lines_deleted
         contributors_data[author_name]['num_lines_edited'] += (num_lines_added + num_lines_deleted)
 
-    return commits_data, contributors_data
+        commit_id += 1
+        if commit_id >= LIMIT:
+            break
+
+    contributors = []
+    contributor_id = 0
+    for k, v in contributors_data.iteritems():
+        contributors.append(
+            {
+                'author_name': k,
+                'id': contributor_id,
+                'num_commits': v['num_commits'],
+                'num_lines_added': v['num_lines_added'],
+                'num_lines_deleted': v['num_lines_deleted'],
+                'num_lines_edited': v['num_lines_edited'],
+            }
+        )
+        contributor_id += 1
+
+    contribs_stats = repo.get_stats_contributors()
+    if contribs_stats is None: contribs_stats = repo.get_stats_contributors()
+
+    return {"num_commits": commit_id, "commits": commits_data}, {"num_contributors": len(contribs_stats), "contributors": contributors}
+
+
+def get_pull_requests(repo):
+    open_prs = repo.get_pulls()
+    closed_prs = repo.get_pulls('closed')
+
+    prs = {}
+    prs_data = {}
+
+    pr_id = 0
+    open_pr_counts = 0
+    closed_pr_counts = 0
+    for pr in open_prs:
+        prs_data[pr_id] = {
+            'title': pr.title,
+            'date': pr.updated_at.strftime("%Y-%m-%d"),
+            'internal_id': pr.number,
+            'open': True
+        }
+        pr_id += 1
+        open_pr_counts += 1
+        if open_pr_counts >= LIMIT:
+            break
+
+    for pr in closed_prs:
+        prs_data[pr_id] = {
+            'title': pr.title,
+            'date': pr.updated_at.strftime("%Y-%m-%d"),
+            'internal_id': pr.number,
+            'open': False
+        }
+        pr_id += 1
+        closed_pr_counts += 1
+        if closed_pr_counts >= LIMIT:
+            break
+
+    prs['num_prs_open'] = open_pr_counts
+    prs['num_prs_closed'] = closed_pr_counts
+    prs['prs'] = prs_data
+    return prs
+
+
+def get_issues(repo):
+    open_issues = repo.get_issues()
+    closed_issues = repo.get_issues(state='closed')
+
+    issues = []
+    issues_data = {}
+
+    issue_id = 0
+    open_issue_count = 0
+    closed_issue_count = 0
+    for issue in open_issues:
+        issues.append(
+            {
+                'title': issue.title,
+                'id': issue_id,
+                'date': issue.created_at.strftime('%Y-%m-%d'),
+                'open': 1
+            }
+        )
+        open_issue_count += 1
+        issue_id += 1
+        if open_issue_count >= LIMIT:
+            break
+
+    for issue in closed_issues:
+        issues.append(
+            {
+                'title': issue.title,
+                'id': issue_id,
+                'date': issue.created_at.strftime('%Y-%m-%d'),
+                'open': 0
+            }
+        )
+        closed_issue_count += 1
+        issue_id += 1
+        if closed_issue_count >= LIMIT:
+            break
+
+    # issues_data['num_issues_open'] = open_issue_count
+    issues_data['num_issues_open'] = repo.open_issues_count
+    issues_data['num_issues_closed'] = closed_issue_count
+    issues_data['issues'] = issues
+
+    return issues_data
+
+
+def get_fork_and_addr(repo):
+    return 1 if repo.fork else 0
+
+
+def get_coin_data(name, id, rank):
+    repo = get_repo(id)
+    res = {}
+
+    res["name"] = name
+    res["rank"] = rank
+    res["id"] = id
+    res["num_stars"] = get_num_stars(repo)
+    res["num_branches"] = get_num_branches(repo)
+    res["num_forks"] = get_num_forks(repo)
+    res["num_watchers"] = get_num_watchers(repo)
+
+    readme, num_lines = get_readme_content_and_lineno(repo)
+    if readme is not None:
+        res["readme_exists"] = 1
+        res["readme_linecount"] = num_lines
+    else:
+        res["readme_exists"] = 0
+        res["readme_linecount"] = 0
+
+    commits, contributors = get_commits_and_contributors(repo)
+    res.update(commits)
+    res.update(contributors)
+
+    prs = get_pull_requests(repo)
+    res.update(prs)
+    issues = get_issues(repo)
+    res.update(issues)
+
+    res['forked'] = get_fork_and_addr(repo)
+
+    return res
