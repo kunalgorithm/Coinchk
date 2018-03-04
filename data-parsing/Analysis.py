e25d4b0b850a343d1d3729be9cebf69c628abea7
breakAwall <kgoel100@gmail.com>
2018-03-03 18:45:38 -0600

    scoring done

--- data-parsing/Analysis.py
+++ data-parsing/Analysis.py
@@ -216,6 +216,18 @@ def open_issues_score(currency_data):
 def currencyScores(input, output):
     overall = {}
     potential = 1120
+
+    with open("top_10coins_data.json", "r") as input_file:
+        data1 = json.load(input_file)
+        input_file.close()
+
+    max_lines = max_lines_edited(data1)
+    max_contr = max_given_key("num_contributors", data1)
+    max_stars = max_given_key("num_stars", data1)
+    max_watchers = max_given_key("num_watchers", data1)
+    max_forks = max_given_key("num_forks", data1)
+    max_readme_line = max_given_key("readme_linecount", data1)
+
     with zipfile.ZipFile(input, "r") as input_file:
         for file in input_file.namelist():
             with input_file.open(file) as f:
@@ -227,13 +239,13 @@ def currencyScores(input, output):
                     currency_name = d[i]["name"]
                     score_attr_1 = open_source_score(currency_data)
                     score_attr_2 = forked_score(currency_data)
-                    score_attr_3 = dev_score(currency_data, max_lines_edited(d))
-                    score_attr_4 = contr_score(currency_data, max_given_key("num_contributors", d))
+                    score_attr_3 = dev_score(currency_data, max_lines)
+                    score_attr_4 = contr_score(currency_data, max_contr)
                     score_attr_5 = interest_score(currency_data,
-                                                        max_given_key("num_stars", d)
-                                                        + max_given_key("num_watchers", d)
-                                                        + max_given_key("num_forks", d))
-                    score_attr_6 = readme_score(currency_data, max_given_key("readme_linecount", d))
+                                                        max_stars
+                                                        + max_watchers
+                                                        + max_forks)
+                    score_attr_6 = readme_score(currency_data, max_readme_line)
                     score_attr_7 = open_issues_score(currency_data)
                     final_score = ((score_attr_1 + score_attr_2 + score_attr_3 +
                                     score_attr_4 + score_attr_5 + score_attr_6 + score_attr_7) / potential) * 100
